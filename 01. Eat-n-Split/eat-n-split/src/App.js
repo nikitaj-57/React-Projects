af96da45665ddc95516a6faf1a839262371576f6
nikitaj-57 <nikitajit2@gmail.com>
2023-06-30 08:28:07 +0530

    Created FriendsList, Friend, FormAddFriend, FormSplitBill components

--- 01. Eat-n-Split/eat-n-split/src/App.js
+++ 01. Eat-n-Split/eat-n-split/src/App.js
@@ -1,24 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// import FriendsList from "./FriendsList";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
 
 function App() {
+  const [addFriend, setAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleAddNewFriend(friend) {
+    setFriends([...friends, friend]);
+    setAddFriend(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList friends={friends} />
+        {addFriend && <FormAddFriend onAddFriend={handleAddNewFriend} />}
+        <Button
+          onClick={() => {
+            setAddFriend((addFriend) => !addFriend);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {addFriend ? "Close" : "Add friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
     </div>
+  );
+}
+
+function FriendsList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance >= 0 ? (
+        friend.balance === 0 ? (
+          <p>You and {friend.name} are even</p>
+        ) : (
+          <p className="green">
+            {friend.name} owes you €{friend.balance}
+          </p>
+        )
+      ) : (
+        <p className="red">
+          You owe {friend.name} €{Math.abs(friend.balance)}
+        </p>
+      )}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+
+    if (!name || !image) return;
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+  }
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>👯‍♀️ Friend name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>🌄 Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with $X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍‍♀️ Your expense</label>
+      <input type="text" />
+
+      <label>👭 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
 
---
+++ 01. Eat-n-Split/eat-n-split/src/BillSplit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BillSplit = () => {
+  return <div>BillSplit</div>;
+};
+
+export default BillSplit;
